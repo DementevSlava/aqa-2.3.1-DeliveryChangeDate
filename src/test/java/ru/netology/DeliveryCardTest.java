523cdfd5045a5c8ceb7bc3973ea1bfa416084d10
Dementev Slava <kursant344@gmail.com>
2020-06-09 12:02:02 +0300

    fix2 test

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -15,7 +15,7 @@ public class DeliveryCardTest {
     String name = dataGenerator.nameByCard();
     String city = dataGenerator.getRandomCity();
     String phone = dataGenerator.getPhone();
-    SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+    SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
 
     @BeforeEach
     void setUp() {
